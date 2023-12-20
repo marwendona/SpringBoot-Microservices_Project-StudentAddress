e87dfa374b3286cd8e9bb07149f76c5fc52d0936
Marwen Kammoun <marwenkammoun3@gmail.com>
2023-12-20 23:46:20 +0100

    add CreateStudentRequest

---
+++ university-monolithic-app/src/main/java/tn/iit/request/CreateStudentRequest.java
@@ -0,0 +1,55 @@
+package tn.iit.request;
+
+public class CreateStudentRequest {
+
+	private String firstName;
+
+	private String lastName;
+
+	private String email;
+
+	private String street;
+
+	private String city;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+}
