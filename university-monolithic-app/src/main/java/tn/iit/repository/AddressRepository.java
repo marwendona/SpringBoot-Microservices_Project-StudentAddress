3cd285ca795840efb2c401b389e0ed0bf0e66c00
Marwen Kammoun <marwenkammoun3@gmail.com>
2023-12-20 23:47:58 +0100

    add AddressRepository

---
+++ university-monolithic-app/src/main/java/tn/iit/repository/AddressRepository.java
@@ -0,0 +1,11 @@
+package tn.iit.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import tn.iit.entity.Address;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
+}
