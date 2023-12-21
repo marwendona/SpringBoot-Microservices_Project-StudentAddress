c40e824836436b5b82f6bc4b6c2a7940367a9c1c
Marwen Kammoun <marwenkammoun3@gmail.com>
2023-12-21 01:36:37 +0100

    add CreateAddressRequest

---
+++ student-service/src/main/java/tn/iit/studentservice/request/CreateAddressRequest.java
@@ -0,0 +1,23 @@
+package tn.iit.studentservice.request;
+
+public class CreateAddressRequest {
+    private String street;
+
+    private String city;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
