1e156ea812e184c3f2de5e492ed876859504a267
Marwen Kammoun <marwenkammoun3@gmail.com>
2023-12-21 01:35:50 +0100

    add Address

---
+++ adress-service/src/main/java/tn/iit/adressservice/entity/Address.java
@@ -0,0 +1,44 @@
+package tn.iit.adressservice.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "address")
+public class Address {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "street")
+	private String street;
+
+	@Column(name = "city")
+	private String city;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+}
