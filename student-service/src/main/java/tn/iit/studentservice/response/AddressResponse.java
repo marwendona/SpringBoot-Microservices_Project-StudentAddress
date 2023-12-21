d3e34035f9cb69ea49bb9b18deed42ecfbfe3290
Marwen Kammoun <marwenkammoun3@gmail.com>
2023-12-21 01:39:26 +0100

    add AddressResponse

---
+++ student-service/src/main/java/tn/iit/studentservice/response/AddressResponse.java
@@ -0,0 +1,36 @@
+package tn.iit.studentservice.response;
+
+
+public class AddressResponse {
+
+	private Long id;
+	private String street;
+
+	private String city;
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+}
