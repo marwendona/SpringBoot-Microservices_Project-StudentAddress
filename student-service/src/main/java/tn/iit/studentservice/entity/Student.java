03281e9d937964bc08a54bec1d5ae12725d81957
Marwen Kammoun <marwenkammoun3@gmail.com>
2023-12-21 01:32:12 +0100

    add Student

---
+++ student-service/src/main/java/tn/iit/studentservice/entity/Student.java
@@ -0,0 +1,65 @@
+package tn.iit.studentservice.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "student")
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	@Column(name = "email")
+	private String email;
+
+	private Long address;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Long getAddress() {
+		return address;
+	}
+
+	public void setAddress(Long address) {
+		this.address = address;
+	}
+
+}
